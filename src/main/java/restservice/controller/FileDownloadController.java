bedbb9eec0102d9d582df62d6cf41b36362880de
Vitaliy Arkhanhelski <vit667@gmail.com>
2020-09-05 01:00:02 +0200

    Initial commit

---
+++ src/main/java/restservice/controller/FileDownloadController.java
@@ -0,0 +1,43 @@
+package restservice.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import restservice.service.FileStorageService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+
+@RestController
+public class FileDownloadController {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileDownloadController.class);
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @GetMapping("/downloadFile/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile (@PathVariable String fileName, HttpServletRequest request){
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+
+        String contentType=null;
+        try{
+            contentType=request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+           logger.info("Could not determine file type");
+        }
+        if (contentType==null) contentType="application/octet-stream";
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\""+resource.getFilename()+"\"")
+                .body(resource);
+    }
+}
